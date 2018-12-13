c9840fc98c1e7d19bd1c671d433065125dc4a2ff
scarger <supremeterminal@gmail.com>
2018-12-12 21:56:47 -0500

    initial commit

---
+++ lib/group_feed.dart
@@ -0,0 +1,11 @@
+/// group_feed
+///
+/// A Aqueduct web server.
+library group_feed;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
